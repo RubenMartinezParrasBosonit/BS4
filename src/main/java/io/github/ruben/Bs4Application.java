fa2b1c522be8cf9aee27773bec206675d2350365
Rubén Martínez Parras <ruben.mparras@bosonit.com>
2022-02-15 13:49:20 +0100

    Parte 2 del ejercicio BS4 terimado

--- src/main/java/io/github/ruben/Bs4Application.java
+++ src/main/java/io/github/ruben/Bs4Application.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
 
--- src/main/java/io/github/ruben/controladores/ControladorGET.java
+++ src/main/java/io/github/ruben/controladores/ControladorGET.java
@@ -3,6 +3,7 @@ package io.github.ruben.controladores;
 import io.github.ruben.configuracion.PropiedadesSimples;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
